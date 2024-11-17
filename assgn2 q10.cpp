07d93cb448fef42277131a9e042625066631c531
24csu067 <24csu067@ncuindia.edu>
2024-11-17 21:14:55 +0530

    Update assgn2 q10.cpp

--- assgn2 q10.cpp
+++ assgn2 q10.cpp
@@ -4,10 +4,10 @@
 int isPrime(int num) {
     if (num <= 1)
     return 0;
-    for (int i = 2; i <= num ; i++) {
-        if (num % i == 0) return 1;
+    for (int i = 2; i <= num/2 ; i++) {
+        if (num % i == 0) return 0;
     }
-    return 0;
+    return 1;
 }
 
 // Function to count prime numbers in an array
